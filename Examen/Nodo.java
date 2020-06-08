86279667d754f27a6ac1558bb8b52d29e78851c8
Jeronimojr1 <jeronimomargaritocruz@gmail.com>
2020-06-08 17:32:05 -0500

    Clase Nodo

---
+++ Examen/Nodo.java
@@ -0,0 +1,18 @@
+package Examen;
+public class Nodo {
+    public Nodo Siguiente;
+    private int Data;
+    public Nodo(Nodo Sig, int Data){
+       this.Siguiente = Sig;
+       this.Data = Data;
+    }
+    public int obtenerData(){
+        return Data;
+    }
+    public void colocarDara(int Data){
+        this.Data = Data;
+    }
+    public String toString(){
+        return "Data : "+Data;
+    }
+}
