fb4f3c272be73098d101c247025ab8b947974bd6
Jeronimojr1 <jeronimomargaritocruz@gmail.com>
2020-06-10 12:27:41 -0500

    Clase Prueba Arbol

---
+++ EjercicioArbol/PruebaArbol.java
@@ -0,0 +1,75 @@
+package EjercicioArbol;
+public class PruebaArbol {
+    
+    public static void main(String[] args) {
+        Arbol bonsai = new Arbol();
+        bonsai.insertar(new Nodo(10));
+        if(bonsai.buscar(new Nodo(10))){
+            System.out.println("EL 10 SI SE ENCUENTRA");
+        }
+        ////////////////////////////////////////////////////
+        Nodo AB;
+        ////////////////////////////////////////////////////
+        AB= new Nodo(0);
+        if(bonsai.buscar(AB)){
+            System.out.println("EL "+AB+" SI SE ENCUENTRA");
+        }else{
+            System.out.println("EL "+AB+" NO SE ENCUENTRA");
+        }
+        ////////////////////////////////////////////////////
+        AB= new Nodo(5);
+        bonsai.insertar(AB);
+        if(bonsai.buscar(AB)){
+            System.out.println("EL "+AB+" SI SE ENCUENTRA");
+        }else{
+            System.out.println("EL "+AB+" NO SE ENCUENTRA");
+        }
+        ////////////////////////////////////////////////////
+        AB= new Nodo(15);
+        bonsai.insertar(AB);
+        if(bonsai.buscar(AB)){
+            System.out.println("EL "+AB+" SI SE ENCUENTRA");
+        }else{
+            System.out.println("EL "+AB+" NO SE ENCUENTRA");
+        }
+        ////////////////////////////////////////////////////
+        AB= new Nodo(4);
+        bonsai.insertar(AB);
+        if(bonsai.buscar(AB)){
+            System.out.println("EL "+AB+" SI SE ENCUENTRA");
+        }else{
+            System.out.println("EL "+AB+" NO SE ENCUENTRA");
+        }
+        ////////////////////////////////////////////////////
+        AB= new Nodo(16);
+        bonsai.insertar(AB);
+        if(bonsai.buscar(AB)){
+            System.out.println("EL "+AB+" SI SE ENCUENTRA");
+        }else{
+            System.out.println("EL "+AB+" NO SE ENCUENTRA");
+        }    
+        ////////////////////////////////////////////////////
+        AB= new Nodo(8);
+        bonsai.insertar(AB);
+        if(bonsai.buscar(AB)){
+            System.out.println("EL "+AB+" SI SE ENCUENTRA");
+        }else{
+            System.out.println("EL "+AB+" NO SE ENCUENTRA");
+        }    
+        ////////////////////////////////////////////////////
+        AB= new Nodo(11);
+        bonsai.insertar(AB);
+        if(bonsai.buscar(AB)){
+            System.out.println("EL "+AB+" SI SE ENCUENTRA");
+        }else{
+            System.out.println("EL "+AB+" NO SE ENCUENTRA");
+        }    
+        ////////////////////////////////////////////////////
+        AB= new Nodo(1111);
+        if(bonsai.buscar(AB)){
+            System.out.println("EL "+AB+" SI SE ENCUENTRA");
+        }else{
+            System.out.println("EL "+AB+" NO SE ENCUENTRA");
+        }    
+    } 
+}
